49a34dc6dcdaf26eb6b47ecdcdd9cc1a64e5682e
John Alexander Arguello Ospina <macbook@macbooks-MacBook-Air.local>
2023-01-22 23:30:23 +1030

    se sube versión inicial front del proyecto

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+          Let's start the business app
+        </p>
+      </header>
+      <Button> Test button</Button>
+      
+
+    </div>
+  );
+}
+
+export default App;
