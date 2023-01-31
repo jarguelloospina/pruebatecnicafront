4337505f4d1f5e9dfdd2133d2343ade813e21817
John Alexander Arguello Ospina <macbook@macbooks-MacBook-Air.local>
2023-01-31 20:25:05 +1030

    segunda versión sin NavBar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
-
+import Navbar from './components/layout/Navbar';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
         <p>
           Let's start the business app
         </p>
       </header>
-      <Button> Test button</Button>
-      
 
+      <Navbar/>
+      <Button> Test button</Button>
+    
     </div>
   );
 }
